3fa953ec2a84f9795192d4d42eaa125eef06e5ef
Elysia <1678310645@qq.com>
2025-06-29 19:54:10 +0800

    更新了音效

--- Mainmenu/mainmenu.gd
+++ Mainmenu/mainmenu.gd
@@ -5,6 +5,8 @@ extends Node2D
 @onready var panel_3: Panel = $Panel3
 @onready var panel_4: Panel = $Panel4
 @onready var panel_5: Panel = $Panel5
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var audio_stream_player_2: AudioStreamPlayer = $AudioStreamPlayer2
 
 func _process(delt) :
 	pass
@@ -16,12 +18,15 @@ func _ready():
 	panel_3.visible=false
 	panel_4.visible=false
 	panel_5.visible=false
+	audio_stream_player = get_node_or_null("AudioStreamPlayer")
+
+	
 
 
 func _on_play_button_down() -> void:
 	panel_2.visible=true
 	v_box_container.visible=false
-	
+	audio_stream_player.play()
 
 
 
@@ -29,14 +34,17 @@ func _on_setting_button_down() -> void:
 	
 	v_box_container.visible=false
 	panel.visible=true
+	audio_stream_player.play()
 
 
 func _on_exit_button_down() -> void:
 	get_tree().quit()
+	audio_stream_player.play()
 
 
 func _on_back_pressed() -> void:
 	_ready()
+	audio_stream_player.play()
 
 
 # 使用预加载优化
@@ -46,32 +54,38 @@ func _on_twopeople_pressed() -> void:
 	# 确保这行使用制表符缩进
 	panel_2.visible=false
 	panel_4.visible=true
+	audio_stream_player.play()
 
 
 func _on_onepeople_pressed() -> void:
 	# 确保这行使用制表符缩进
 	panel_2.visible=false
 	panel_4.visible=true
+	audio_stream_player.play()
 
 
 func _on_back1_pressed() -> void:
 	panel_2.visible=false
 	v_box_container.visible=true
+	audio_stream_player.play()
 
 
 func _on_play2_pressed() -> void:
 	v_box_container.visible=false
 	panel_3.visible=true
+	audio_stream_player.play()
 
 
 func _on_back_5_pressed() -> void:
 	v_box_container.visible=true
 	panel_3.visible=false
+	audio_stream_player_2.play()
 
 
 func _on_back_2_pressed() -> void:
 	panel_4.visible=false
 	panel_5.visible=true
+	audio_stream_player_2.play()
 
 
 func _on_back_3_pressed() -> void:
